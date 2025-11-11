e1e4cf31fd1eef17c466d676b6ad30a8faf31a5c
Alex Chebotarsky <alexchebotarsky@gmail.com>
2025-11-11 23:48:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zigbee
+
+Built with [ESP-IDF](https://github.com/espressif/esp-idf)
---
+++ main/app_main.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+
+#include "sdkconfig.h"
+
+extern "C" void app_main(void) { printf("Hello World!\n"); }
