4da9eaf4ec577753a3ae5ce1eb2522755dbeea26
Alex Chebotarsky <alexchebotarsky@gmail.com>
2026-09-18 23:37:38 +0200

    Add back light

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -5,9 +5,10 @@
 #include "SingleLED.hpp"
 #include "Storage.hpp"
 #include "TemperatureSensor.hpp"
+#include "ZigbeeDevice.hpp"
+#include "ZigbeeStack.hpp"
 #include "esp_err.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "esp_zigbee_core.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 
@@ -22,6 +23,57 @@ SingleLED led(CONFIG_INBUILT_LED_GPIO);
 
 TemperatureSensor temperature_sensor(CONFIG_TEMPERATURE_SENSOR_SDA_GPIO,
                                      CONFIG_TEMPERATURE_SENSOR_SCL_GPIO);
+
+ZigbeeDevice light_device(DeviceConfig{
+    .endpoint = CONFIG_LIGHT_ENDPOINT,
+    .app_device_id = ESP_ZB_HA_ON_OFF_LIGHT_DEVICE_ID,
+    .power_source = ESP_ZB_ZCL_BASIC_POWER_SOURCE_BATTERY,
+    .manufacturer = CONFIG_DEVICE_MANUFACTURER,
+    .model = CONFIG_DEVICE_MODEL,
+});
+
+esp_err_t setup_clusters(esp_zb_cluster_list_t* clusters) {
+  esp_zb_on_off_cluster_cfg_t on_off_cfg = {
+      .on_off = storage.get_active(),
+  };
+  auto* on_off_attrs = esp_zb_on_off_cluster_create(&on_off_cfg);
+  esp_err_t err = esp_zb_cluster_list_add_on_off_cluster(
+      clusters, on_off_attrs, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+  if (err != ESP_OK) {
+    return err;
+  }
+
+  esp_zb_level_cluster_cfg_t level_cfg = {
+      .current_level =
+          static_cast<uint8_t>(std::round(storage.get_brightness() * 254.0)),
+  };
+  auto* level_attrs = esp_zb_level_cluster_create(&level_cfg);
+  err = esp_zb_cluster_list_add_level_cluster(clusters, level_attrs,
+                                              ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+  if (err != ESP_OK) {
+    return err;
+  }
+
+  esp_zb_color_cluster_cfg_t color_cfg = {
+      .current_x =
+          static_cast<uint16_t>(std::round(storage.get_color_x() * 65535.0)),
+      .current_y =
+          static_cast<uint16_t>(std::round(storage.get_color_y() * 65535.0)),
+      .color_mode = ESP_ZB_ZCL_COLOR_CONTROL_COLOR_MODE_DEFAULT_VALUE,
+      .options = ESP_ZB_ZCL_COLOR_CONTROL_OPTIONS_DEFAULT_VALUE,
+      .enhanced_color_mode =
+          ESP_ZB_ZCL_COLOR_CONTROL_ENHANCED_COLOR_MODE_DEFAULT_VALUE,
+      .color_capabilities = 0x0008,
+  };
+  auto* color_attrs = esp_zb_color_control_cluster_create(&color_cfg);
+  err = esp_zb_cluster_list_add_color_control_cluster(
+      clusters, color_attrs, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+  if (err != ESP_OK) {
+    return err;
+  }
+
+  return ESP_OK;
+}
 
 extern "C" void app_main(void) {
   esp_err_t err = nvs_flash_init();
@@ -49,14 +101,89 @@ extern "C" void app_main(void) {
     esp_restart();
   }
 
-  while (true) {
-    led.set_active(true);
+  err = Zigbee.init();
+  if (err != ESP_OK) {
+    printf("Error initializing ZigbeeStack: %s\n", esp_err_to_name(err));
+    return;
+  }
 
-    TemperatureReading reading = temperature_sensor.read();
-    printf("Temperature: %.2f °C / Humidity: %.2f\n", reading.temperature,
-           reading.humidity);
+  err = light_device.init(setup_clusters);
+  if (err != ESP_OK) {
+    printf("Error initializing ZigbeeDevice: %s\n", esp_err_to_name(err));
+    return;
+  }
 
-    led.set_active(false);
-    vTaskDelay(pdMS_TO_TICKS(5000));
+  light_device.handle_action<esp_zb_zcl_set_attr_value_message_t>(
+      ESP_ZB_ZCL_CLUSTER_ID_ON_OFF, ESP_ZB_CORE_SET_ATTR_VALUE_CB_ID,
+      [](const auto* msg) {
+        switch (msg->attribute.id) {
+          case ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID: {
+            bool active = *static_cast<bool*>(msg->attribute.data.value);
+
+            esp_err_t err = storage.set_active(active);
+            if (err != ESP_OK) return err;
+
+            return led.set_active(active);
+          }
+          default:
+            printf("Unsupported action: cluster_id=%u, attribute_id=%u\n",
+                   msg->info.cluster, msg->attribute.id);
+            return ESP_ERR_NOT_SUPPORTED;
+        }
+      });
+
+  light_device.handle_action<esp_zb_zcl_set_attr_value_message_t>(
+      ESP_ZB_ZCL_CLUSTER_ID_LEVEL_CONTROL, ESP_ZB_CORE_SET_ATTR_VALUE_CB_ID,
+      [](const auto* msg) {
+        switch (msg->attribute.id) {
+          case ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_CURRENT_LEVEL_ID: {
+            double brightness =
+                *static_cast<uint8_t*>(msg->attribute.data.value) / 254.0;
+
+            esp_err_t err = storage.set_brightness(brightness);
+            if (err != ESP_OK) return err;
+
+            return led.set_brightness(brightness);
+          }
+          default:
+            printf("Unsupported action: cluster_id=%u, attribute_id=%u\n",
+                   msg->info.cluster, msg->attribute.id);
+            return ESP_ERR_NOT_SUPPORTED;
+        }
+      });
+
+  light_device.handle_action<esp_zb_zcl_set_attr_value_message_t>(
+      ESP_ZB_ZCL_CLUSTER_ID_COLOR_CONTROL, ESP_ZB_CORE_SET_ATTR_VALUE_CB_ID,
+      [](const auto* msg) {
+        switch (msg->attribute.id) {
+          case ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_X_ID: {
+            double x =
+                *static_cast<uint16_t*>(msg->attribute.data.value) / 65535.0;
+
+            esp_err_t err = storage.set_color_x(x);
+            if (err != ESP_OK) return err;
+
+            return led.set_color(x, led.get_color().y);
+          }
+          case ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_Y_ID: {
+            double y =
+                *static_cast<uint16_t*>(msg->attribute.data.value) / 65535.0;
+
+            esp_err_t err = storage.set_color_y(y);
+            if (err != ESP_OK) return err;
+
+            return led.set_color(led.get_color().x, y);
+          }
+          default:
+            printf("Unsupported action: cluster_id=%u, attribute_id=%u\n",
+                   msg->info.cluster, msg->attribute.id);
+            return ESP_ERR_NOT_SUPPORTED;
+        }
+      });
+
+  err = Zigbee.start();
+  if (err != ESP_OK) {
+    printf("Error starting Zigbee: %s\n", esp_err_to_name(err));
+    return;
   }
 }
